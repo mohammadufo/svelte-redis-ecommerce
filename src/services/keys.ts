e9748a6d9901b46487702c3333083174ec4ebcfe
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-19 14:36:39 +0330

    impelemnt create item

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,4 @@
 export const pageCacheKey = (id: string) => `pageCache#${id}`;
 export const usersKey = (id: string) => `users#${id}`;
 export const sessionKey = (id: string) => `sessions#${id}`;
+export const itemKey = (id: string) => `items#${id}`;
