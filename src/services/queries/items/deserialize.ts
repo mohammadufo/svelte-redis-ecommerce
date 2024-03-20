f7064111c8184a5147299ea2a151c807fc567d6b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-21 00:59:22 +0330

    impelement get many items by pipe line

--- src/services/queries/items/deserialize.ts
+++ src/services/queries/items/deserialize.ts
@@ -1,4 +1,19 @@
 import type { Item } from '$services/types';
 import { DateTime } from 'luxon';
 
-export const deserialize = (id: string, item: { [key: string]: string }): Item => {};
+export const deserialize = (id: string, item: { [key: string]: string }): Item => {
+	return {
+		id,
+		name: item.name,
+		description: item.description,
+		highestBidUserId: item.highestBidUserId,
+		ownerId: item.ownerId,
+		imageUrl: item.imageUrl,
+		createdAt: DateTime.fromMillis(+item.createdAt),
+		endingAt: DateTime.fromMillis(+item.endingAt),
+		views: +item.views,
+		likes: +item.likes,
+		price: +item.price,
+		bids: +item.bids
+	};
+};
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,11 +2,34 @@ import { itemKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { deserialize } from './deserialize';
 import { serialize } from './serialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemKey(id));
 
-export const getItems = async (ids: string[]) => {};
+	if (!Object.keys(item).length) {
+		return null;
+	}
+
+	return deserialize(id, item);
+};
+
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemKey(id));
+	});
+
+	const results = await Promise.all(commands);
+
+	return results?.map((item, index) => {
+		if (!Object?.keys(item)?.length) {
+			return null;
+		}
+
+		return deserialize(ids[index], item);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
