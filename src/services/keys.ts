079080937fa0a5154b953993d00ea3e373f9fcd9
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-21 23:02:10 +0330

    add like and dislike functionality

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,3 +3,4 @@ export const usersKey = (id: string) => `users#${id}`;
 export const sessionKey = (id: string) => `sessions#${id}`;
 export const itemKey = (id: string) => `items#${id}`;
 export const usernameUniqueKey = () => 'username:unique';
+export const userLikesKey = (id: string) => `user:likes#${id}`;
--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,26 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { itemKey, userLikesKey } from '$services/keys';
+import { client } from '$services/redis';
+
+export const userLikesItem = async (itemId: string, userId: string) => {
+	return client.sIsMember(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+	if (inserted) {
+		return client.hIncrBy(itemKey(itemId), 'likes', 1);
+	}
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+	const isRemoved = await client.sRem(userLikesKey(userId), itemId);
+
+	if (isRemoved) {
+		return client.hIncrBy(itemKey(itemId), 'likes', -1);
+	}
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
