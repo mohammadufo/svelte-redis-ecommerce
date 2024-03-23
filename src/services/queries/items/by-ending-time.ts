458349a898a35493f0d8d5fc1f988aaeccac28fb
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-23 20:13:04 +0330

    implement end soonest slider

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,17 @@
-export const itemsByEndingTime = async (
-	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+import { itemKey, itemsByEndingAtKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
+export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	const ids = await client.zRange(itemsByEndingAtKey(), Date.now(), '+inf', {
+		BY: 'SCORE',
+		LIMIT: {
+			offset,
+			count
+		}
+	});
+
+	const results = await Promise.all(ids.map((_id) => client.hGetAll(itemKey(_id))));
+
+	return results.map((item, i) => deserialize(ids[i], item));
+};
