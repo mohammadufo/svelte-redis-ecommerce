aac29070b3b629a95140a0d8a10147186580e724
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-24 00:29:18 +0330

    impelement most views slider

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,33 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { itemKey, itemsByViewsKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let results: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemKey('*')}->name`,
+			`${itemKey('*')}->views`,
+			`${itemKey('*')}->imageUrl`,
+			`${itemKey('*')}->endingAt`,
+			`${itemKey('*')}->price`
+		],
+		BY: 'nosort',
+		DIRECTION: order,
+		LIMIT: {
+			count,
+			offset
+		}
+	});
+
+	const items = [];
+
+	while (results.length) {
+		const [id, name, views, imageUrl, endingAt, price, ...rest] = results;
+		const item = deserialize(id, { name, views, imageUrl, endingAt, price });
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
+};
